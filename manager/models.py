352bdcb46e02861a44ddced9b1536f906a464ad6
Tyler Lam <tylerl264@gmail.com>
2021-12-14 17:43:24 -0700

    refactor: Clean up code and create form validation

--- manager/models.py
+++ manager/models.py
@@ -1,11 +1,17 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 from manager.utils.constants import MAX_EMAIL_LENGTH, MAX_NAME_LENGTH, MAX_PHONE_NUM_LENGTH
 
-# Create your models here.
+phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+000000000'.")
+email_regex = RegexValidator(regex=r'\w[\w\.-]*@\w[\w\.-]+\.\w+', message="Email must be entered in the format: 'tyler@hogwarts.edu.")
+
+"""
+Teammate model for storing information regarding teammates.
+"""
 class Teammate(models.Model):
     first_name = models.CharField(max_length=MAX_NAME_LENGTH)
     last_name = models.CharField(max_length=MAX_NAME_LENGTH)
-    phone_number = models.CharField(max_length=MAX_PHONE_NUM_LENGTH)
-    email = models.CharField(max_length=MAX_EMAIL_LENGTH)
+    phone_number = models.CharField(validators=[phone_regex], max_length=MAX_PHONE_NUM_LENGTH)
+    email = models.CharField(validators=[email_regex], max_length=MAX_EMAIL_LENGTH)
     is_admin = models.BooleanField(default=False)
